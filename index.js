dae8bcc4758b093a7041911e7af5fe23b85f4fb7
pr1moz <primozz.kos@gmail.com>
2022-07-24 16:55:40 +0200

    Add check interval

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
+const interval = 30000; // 5 min
 const bikeURL =
   'https://www.canyon.com/en-si/road-bikes/endurance-bikes/endurace/al/endurace-7/2733.html?dwvar_2733_pv_rahmenfarbe=BK%2FBU';
 const bikeSize = 'L';
@@ -10,24 +11,26 @@ const bikeSize = 'L';
 app.listen(3000, () => {
   console.log('Starting monitor...\n');
 
-  fetch(bikeURL)
-    .then((response) => {
-      return response.text();
-    })
-    .then((html) => {
-      const { document } = new JSDOM(html).window;
-      const buyButton = document.querySelector(
-        `.productConfiguration__optionListItem .productConfiguration__selectVariant[data-product-size="${bikeSize}"]`
-      ).innerHTML;
-      const isInStock = !buyButton.includes('Notify');
+  setInterval(() => {
+    fetch(bikeURL)
+      .then((response) => {
+        return response.text();
+      })
+      .then((html) => {
+        const { document } = new JSDOM(html).window;
+        const buyButton = document.querySelector(
+          `.productConfiguration__optionListItem .productConfiguration__selectVariant[data-product-size="${bikeSize}"]`
+        ).innerHTML;
+        const isInStock = !buyButton.includes('Notify');
 
-      if (isInStock) {
-        console.info('BIKE IS IN STOCK! HURRY UP!');
-      } else {
-        console.log('Bike is not in stock :(');
-      }
-    })
-    .catch((error) => {
-      console.error('There seems to be an error fetching Canyon website -', error);
-    });
+        if (isInStock) {
+          console.info('BIKE IS IN STOCK! HURRY UP!');
+        } else {
+          console.log('Bike is not in stock :(');
+        }
+      })
+      .catch((error) => {
+        console.error('There seems to be an error fetching Canyon website -', error);
+      });
+  }, interval);
 });
