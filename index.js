b8bb826226de022fda3262502eba96a448324d66
pr1moz <primozz.kos@gmail.com>
2022-07-25 18:32:26 +0200

    Add telegram integration

--- index.js
+++ index.js
@@ -4,33 +4,67 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const interval = 30000; // 5 min
-const bikeURL =
-  'https://www.canyon.com/en-si/road-bikes/endurance-bikes/endurace/al/endurace-7/2733.html?dwvar_2733_pv_rahmenfarbe=BK%2FBU';
+const bikeURLs = [
+  'https://www.canyon.com/en-si/road-bikes/endurance-bikes/endurace/al/endurace-7/2733.html?dwvar_2733_pv_rahmenfarbe=BK%2FBU',
+  'https://www.canyon.com/en-si/road-bikes/endurance-bikes/endurace/al/endurace-7/2733.html?dwvar_2733_pv_rahmenfarbe=BK%2FBK',
+];
 const bikeSize = 'L';
+
+const telegramURL = 'https://api.telegram.org/bot';
+const telegramToken = 'XXXXXXXXX';
+const chatID = 'XXXXXX';
+
+let lastUptimeCheckDate = Date.now();
 
 app.listen(3000, () => {
   console.log('Starting monitor...\n');
+  sendMessage('Monitor started');
 
   setInterval(() => {
-    fetch(bikeURL)
-      .then((response) => {
-        return response.text();
-      })
-      .then((html) => {
-        const { document } = new JSDOM(html).window;
-        const buyButton = document.querySelector(
-          `.productConfiguration__optionListItem .productConfiguration__selectVariant[data-product-size="${bikeSize}"]`
-        ).innerHTML;
-        const isInStock = !buyButton.includes('Notify');
+    let isInStock = false;
 
-        if (isInStock) {
-          console.info('BIKE IS IN STOCK! HURRY UP!');
-        } else {
-          console.log('Bike is not in stock :(');
-        }
-      })
-      .catch((error) => {
-        console.error('There seems to be an error fetching Canyon website -', error);
-      });
+    // Status update every 12 hours
+    if (Date.now() - lastUptimeCheckDate > 43200000) {
+      lastUptimeCheckDate = Date.now();
+      sendMessage('Monitor is still working');
+      if (!isInStock) {
+        sendMessage('Bike is still not in stock :(');
+      }
+    }
+
+    bikeURLs.forEach((url) => {
+      fetch(url)
+        .then((response) => {
+          return response.text();
+        })
+        .then((html) => {
+          const { document } = new JSDOM(html).window;
+          const buyButton = document.querySelector(
+            `.productConfiguration__optionListItem .productConfiguration__selectVariant[data-product-size="${bikeSize}"]`
+          ).innerHTML;
+          isInStock = !buyButton.includes('Notify');
+
+          if (isInStock) {
+            console.info('The bike is in stock!');
+            sendMessage('The bike you want is in stock! Hurry up!');
+          }
+        })
+        .catch((error) => {
+          console.error('There seems to be an error fetching Canyon website -', error);
+        });
+    });
   }, interval);
 });
+
+const sendMessage = (message) => {
+  fetch(`${telegramURL}${telegramToken}/sendMessage`, {
+    method: 'post',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      chat_id: chatID,
+      text: message,
+    }),
+  });
+};
